cc9560234b68bac32237dd029bf179019b9e06f7
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-08-11 01:38:20 +0700

    -

--- platform/themes/main/views/news-detail.blade.php
+++ platform/themes/main/views/news-detail.blade.php
@@ -19,8 +19,10 @@
 
                             <p>Hiện tại, Mi 11 Lite 5G l&agrave; một trong những chiếc điện thoại tầm trung 5G đ&aacute;ng mua nhất ở thời điểm hiện tại, v&agrave; hứa hẹn l&agrave; một người bạn đồng h&agrave;nh đ&aacute;ng tin cậy cho c&aacute;c bạn học sinh, sinh vi&ecirc;n.</p>
 
-                            <p>Cụ thể, Mi 11 Lite 5G được nh&agrave; sản xuất&nbsp;<a href="https://cellphones.com.vn/mobile/xiaomi.html" title="Xiaomi">Xiaomi</a>&nbsp;trang bị m&agrave;n h&igrave;nh AMOLED cao cấp, c&oacute; k&iacute;ch thước kh&aacute; lớn l&agrave; 6.5 inch, gi&uacute;p mang tới kh&ocirc;ng
-                                gian hiển thị rộng r&atilde;i cho c&aacute;c bạn học sinh, sinh vi&ecirc;n học online, t&igrave;m t&agrave;i liệu hay giải tr&iacute; sau những giờ học căng thẳng. M&agrave;n h&igrave;nh n&agrave;y c&ograve;n hỗ trợ độ ph&acirc;n giải Full-HD+, HDR10+, c&oacute; tần số qu&eacute;t
+                            <p>Cụ thể, Mi 11 Lite 5G được nh&agrave; sản xuất&nbsp;<a href="https://cellphones.com.vn/mobile/xiaomi.html" title="Xiaomi">Xiaomi</a>&nbsp;trang bị m&agrave;n h&igrave;nh AMOLED cao cấp, c&oacute; k&iacute;ch thước kh&aacute; lớn l&agrave; 6.5 inch, gi&uacute;p mang tới
+                                kh&ocirc;ng
+                                gian hiển thị rộng r&atilde;i cho c&aacute;c bạn học sinh, sinh vi&ecirc;n học online, t&igrave;m t&agrave;i liệu hay giải tr&iacute; sau những giờ học căng thẳng. M&agrave;n h&igrave;nh n&agrave;y c&ograve;n hỗ trợ độ ph&acirc;n giải Full-HD+, HDR10+, c&oacute; tần
+                                số qu&eacute;t
                                 90Hz cũng như tốc độ lấy mẫu cảm ứng 240Hz v&agrave; cũng hỗ trợ Dolby Vision.
                             </p>
 
@@ -28,7 +30,8 @@
                                 <img alt="Sforum - Trang thông tin công nghệ mới nhất Xiaomi-Mi-11-Lite-5G-1 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " data-pin-no-hover="true" height="1365" loading="lazy" sizes="(max-width: 2048px) 100vw, 2048px" src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1.jpg" srcset="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1.jpg 2048w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-600x400.jpg 600w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-960x640.jpg 960w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-768x512.jpg 768w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-1536x1024.jpg 1536w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-750x500.jpg 750w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Xiaomi-Mi-11-Lite-5G-1-1140x760.jpg 1140w" title="Sforum - Trang thông tin công nghệ mới nhất Xiaomi-Mi-11-Lite-5G-1 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " width="2048"/>
                             </p>
 
-                            <p>B&ecirc;n trong, Mi 11 Lite 5G được cung cấp sức mạnh đến từ con chip Snapdragon 780G của Qualcomm, đi k&egrave;m dung lượng RAM 8GB v&agrave; bộ nhớ trong 128GB. Nhờ đ&oacute; m&agrave; hiệu năng chiếc m&aacute;y n&agrave;y sẽ dư sức sức l&yacute; tốt mọi t&aacute;c vụ h&agrave;ng
+                            <p>B&ecirc;n trong, Mi 11 Lite 5G được cung cấp sức mạnh đến từ con chip Snapdragon 780G của Qualcomm, đi k&egrave;m dung lượng RAM 8GB v&agrave; bộ nhớ trong 128GB. Nhờ đ&oacute; m&agrave; hiệu năng chiếc m&aacute;y n&agrave;y sẽ dư sức sức l&yacute; tốt mọi t&aacute;c
+                                vụ h&agrave;ng
                                 ng&agrave;y của người sử dụng. Chưa hết, Xiaomi c&ograve;n t&iacute;ch hợp vi&ecirc;n pin 4,250 mAh cho Mi 11 Lite 5G, hứa hẹn đ&aacute;p ứng tốt 1 ng&agrave;y d&agrave;i sử dụng với cường độ cao. Điện thoại n&agrave;y đi k&egrave;m cụm 3&nbsp;<a href="https://cellphones.com.vn/phu-kien/camera.html" title="camera">
                                     camera
                                 </a>&nbsp;ch&iacute;nh ở mặt lưng với thiết kế giống Mi 11, bao gồm camera ch&iacute;nh 64MP, camera g&oacute;c si&ecirc;u rộng 8MP v&agrave; camera macro 5MP. M&aacute;y c&oacute; th&ecirc;m camera selfie 20MP ở mặt trước.
@@ -40,18 +43,21 @@
 
                             <h2><strong>Vivo V21 5G (gi&aacute; 8.6 triệu đồng)</strong></h2>
 
-                            <p>Giống như Mi 11 Lite 5G, điện thoại&nbsp;<a href="https://cellphones.com.vn/mobile/vivo.html" title="Vivo">Vivo</a>&nbsp;V21 5G cũng được trang bị c&ocirc;ng nghệ mạng di động ti&ecirc;n tiến nhất hiện nay, gi&uacute;p c&aacute;c bạn học sinh, sinh vi&ecirc;n học online kh&ocirc;ng
+                            <p>Giống như Mi 11 Lite 5G, điện thoại&nbsp;<a href="https://cellphones.com.vn/mobile/vivo.html" title="Vivo">Vivo</a>&nbsp;V21 5G cũng được trang bị c&ocirc;ng nghệ mạng di động ti&ecirc;n tiến nhất hiện nay, gi&uacute;p c&aacute;c bạn học sinh, sinh vi&ecirc;n học
+                                online kh&ocirc;ng
                                 bị gi&aacute;n đoạn.
                             </p>
 
-                            <p>Chiếc điện thoại 5G n&agrave;y cũng c&oacute; nhiều ưu điểm kh&aacute;c như hiệu năng mạnh mẽ được cung cấp từ con chip MediaTek Dimensity 800U được kết hợp với RAM 8GB, bộ nhớ trong 128GB. N&oacute; c&oacute; vi&ecirc;n pin 4,000 mAh với hỗ trợ sạc nhanh 33W, hứa hẹn đ&aacute;p
+                            <p>Chiếc điện thoại 5G n&agrave;y cũng c&oacute; nhiều ưu điểm kh&aacute;c như hiệu năng mạnh mẽ được cung cấp từ con chip MediaTek Dimensity 800U được kết hợp với RAM 8GB, bộ nhớ trong 128GB. N&oacute; c&oacute; vi&ecirc;n pin 4,000 mAh với hỗ trợ sạc nhanh 33W, hứa hẹn
+                                đ&aacute;p
                                 ứng tốt một ng&agrave;y d&agrave;i sử dụng của người d&ugrave;ng.</p>
 
                             <p>
                                 <img alt="Sforum - Trang thông tin công nghệ mới nhất Vivo-V21-5G-8 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " data-pin-no-hover="true" height="1024" loading="lazy" sizes="(max-width: 1536px) 100vw, 1536px" src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8.jpg" srcset="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8.jpg 1536w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8-600x400.jpg 600w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8-960x640.jpg 960w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8-768x512.jpg 768w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8-750x500.jpg 750w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/Vivo-V21-5G-8-1140x760.jpg 1140w" title="Sforum - Trang thông tin công nghệ mới nhất Vivo-V21-5G-8 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " width="1536"/>
                             </p>
 
-                            <p>Ngo&agrave;i ra, Vivo V21 5G c&ograve;n sở hữu khả năng chụp ảnh ấn tượng nhờ được trang bị hệ thống 3 camera sau, bao gồm cảm biến 64MP + ống k&iacute;nh 8MP cho g&oacute;c chụp si&ecirc;u rộng + camera macro 2MP. Ở mặt trước, mẫu di động tầm trung của Vivo c&ograve;n sở
+                            <p>Ngo&agrave;i ra, Vivo V21 5G c&ograve;n sở hữu khả năng chụp ảnh ấn tượng nhờ được trang bị hệ thống 3 camera sau, bao gồm cảm biến 64MP + ống k&iacute;nh 8MP cho g&oacute;c chụp si&ecirc;u rộng + camera macro 2MP. Ở mặt trước, mẫu di động tầm trung của Vivo c&ograve;n
+                                sở
                                 hữu camera selfie 44MP cực kỳ chất lượng, hứa hẹn sẽ mang tới cho người d&ugrave;ng những bức ảnh &ldquo;tự sướng&rdquo; lung linh.</p>
 
                             <p>
@@ -60,19 +66,22 @@
 
                             <h2><strong>OnePlus Nord CE 5G (gi&aacute; 7.99 triệu đồng)</strong></h2>
 
-                            <p>OnePlus Nord CE 5G cũng l&agrave; một lựa chọn kh&ocirc;ng tồi cho c&aacute;c bạn học sinh, sinh vi&ecirc;n. Chiếc điện thoại n&agrave;y được trang bị m&agrave;n h&igrave;nh 6.43 inch với thiết kế tr&agrave;n viền ấn tượng, sử dụng tấm nền Fluid AMOLED cao cấp với độ ph&acirc;n
+                            <p>OnePlus Nord CE 5G cũng l&agrave; một lựa chọn kh&ocirc;ng tồi cho c&aacute;c bạn học sinh, sinh vi&ecirc;n. Chiếc điện thoại n&agrave;y được trang bị m&agrave;n h&igrave;nh 6.43 inch với thiết kế tr&agrave;n viền ấn tượng, sử dụng tấm nền Fluid AMOLED cao cấp với độ
+                                ph&acirc;n
                                 giải 2,400 x 1,080 pixel mang lại những h&igrave;nh ảnh hiển thị m&agrave;u sắc sống động, sắc n&eacute;t v&agrave; ch&acirc;n thực tốt hơn hẳn so với m&agrave;n IPS th&ocirc;ng thường.</p>
 
                             <p>
                                 <img alt="Sforum - Trang thông tin công nghệ mới nhất oneplus-nord-ce-5g-28-scaled-1 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " data-pin-no-hover="true" height="1707" loading="lazy" sizes="(max-width: 2560px) 100vw, 2560px" src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1.jpg" srcset="https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1.jpg 2560w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-600x400.jpg 600w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-960x640.jpg 960w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-768x512.jpg 768w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-1536x1024.jpg 1536w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-2048x1366.jpg 2048w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-750x500.jpg 750w, https://cellphones.com.vn/sforum/wp-content/uploads/2021/08/oneplus-nord-ce-5g-28-scaled-1-1140x760.jpg 1140w" title="Sforum - Trang thông tin công nghệ mới nhất oneplus-nord-ce-5g-28-scaled-1 Top 4 smartphone tầm giá 6 đến 10 triệu đồng cho các học sinh, sinh viên &quot;nhà có điều kiện&quot; " width="2560"/>
                             </p>
 
-                            <p>Về hiệu năng, chiếc điện thoại n&agrave;y sẽ giúp bạn thỏa sức giải trí với b&ocirc;̣ vi xử lý Snapdragon 750G. Con chip n&agrave;y sẽ nhận được sự hỗ trợ từ dung lượng RAM l&ecirc;n tới 8GB RAM giúp chạy đa nhi&ecirc;̣m nhanh chóng, và bộ nhớ trong 128GB gi&uacute;p
+                            <p>Về hiệu năng, chiếc điện thoại n&agrave;y sẽ giúp bạn thỏa sức giải trí với b&ocirc;̣ vi xử lý Snapdragon 750G. Con chip n&agrave;y sẽ nhận được sự hỗ trợ từ dung lượng RAM l&ecirc;n tới 8GB RAM giúp chạy đa nhi&ecirc;̣m nhanh chóng, và bộ nhớ trong 128GB
+                                gi&uacute;p
                                 người d&ugrave;ng c&oacute; kh&ocirc;ng gian lưu trữ lớn.</p>
 
                             <p>Chưa hết, OnePlus Nord CE 5G được nh&agrave; sản xuất tạo h&igrave;nh kh&aacute; bắt mắt v&agrave; sang trọng. N&oacute; c&oacute; m&agrave;n h&igrave;nh đục lỗ bắt mắt ở mặt trước, trong khi mặt lưng nổi bật với cụm 3 camera được sắp xếp theo chiều dọc tr&ocirc;ng kh&aacute;
                                 gọn g&agrave;ng. Mẫu&nbsp;<a href="https://cellphones.com.vn/mobile.html" title="smartphone">smartphone</a>&nbsp;tầm trung của
-                                <a href="https://cellphones.com.vn/mobile/oneplus.html" title=" OnePlus">&nbsp;OnePlus</a>&nbsp;c&ograve;n được trang bị hệ thống 3 camera 64MP chất lượng chất lượng ở ph&iacute;a sau, m&aacute;y ảnh selfie 16MP v&agrave; hoạt động tr&ecirc;n giao diện OxygenOS 11 mới
+                                <a href="https://cellphones.com.vn/mobile/oneplus.html" title=" OnePlus">&nbsp;OnePlus</a>&nbsp;c&ograve;n được trang bị hệ thống 3 camera 64MP chất lượng chất lượng ở ph&iacute;a sau, m&aacute;y ảnh selfie 16MP v&agrave; hoạt động tr&ecirc;n giao diện OxygenOS 11
+                                mới
                                 nh&acirc;́t tr&ecirc;n hệ điều h&agrave;nh Android 11.
                             </p>
 
@@ -92,7 +101,8 @@
                             </p>
 
                             <p>Hiệu năng của m&aacute;y cũng kh&aacute; mạnh với chip Snapdragon 720G, kết hợp với RAM 8GB v&agrave; bộ nhớ trong 128GB. C&ugrave;ng với đ&oacute;, khả năng nhiếp ảnh của thiết bị cũng kh&ocirc;ng hề k&eacute;m cạnh so với c&aacute;c sản phẩm kh&aacute;c trong danh s&aacute;ch
-                                n&agrave;y. Cụm 4 camera nằm trong m&ocirc;-đun h&igrave;nh chữ nhật ở ph&iacute;a sau c&oacute; độ ph&acirc;n giải 64MP + 12MP + 5MP + 5MP, camera selfie 32MP c&oacute; thiết kế đục lỗ ở ch&iacute;nh giữa m&agrave;n h&igrave;nh đ&aacute;p ứng được mọi y&ecirc;u cầu chụp
+                                n&agrave;y. Cụm 4 camera nằm trong m&ocirc;-đun h&igrave;nh chữ nhật ở ph&iacute;a sau c&oacute; độ ph&acirc;n giải 64MP + 12MP + 5MP + 5MP, camera selfie 32MP c&oacute; thiết kế đục lỗ ở ch&iacute;nh giữa m&agrave;n h&igrave;nh đ&aacute;p ứng được mọi y&ecirc;u cầu
+                                chụp
                                 ảnh.</p>
 
                             <p>Ở mặt trước, Galaxy A52 được trang bị m&agrave;n h&igrave;nh Super AMOLED c&oacute; k&iacute;ch thước 6.5 inch, hỗ trợ độ ph&acirc;n giải Full-HD+ (2,400 x 1080 pixel), tỷ lệ khung h&igrave;nh 20:9, độ s&aacute;ng 800 nits v&agrave; tốc độ l&agrave;m tươi 90Hz si&ecirc;u
@@ -106,19 +116,20 @@
 
                         <div class="post__tags">
                             <ul>
+                                <li><strong class="font14">Thẻ:</strong></li>
                                 <li>
                                     <a>
-                                        <span>Samsung Galaxy A52</span>
+                                        <span class="font14">Samsung Galaxy A52</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a>
-                                        <span>Xiaomi 11 Lite 5G</span>
+                                        <span class="font14">Xiaomi 11 Lite 5G</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a>
-                                        <span>Vivo V21 5G</span>
+                                        <span class="font14">Vivo V21 5G</span>
                                     </a>
                                 </li>
                             </ul>
@@ -130,15 +141,16 @@
                             </div>
                             <div class="post__related">
                                 @for ($i = 0; $i < 6; $i++)
-                                    <a href="javascript:void(0);" class="post__related__item">
-                                        <span class="post__related__item--img">
+                                    <div class="post__related__item">
+                                        <a href="javascript:void(0);" class="post__related__item--img">
                                             <img alt="News" class="img-fluid w-100" src="{{ Theme::asset()->url("images/related.jpg") }}"/>
-                                        </span>
-                                        <span class="post__related__item--content">
-                                            <span class="post__related__item--title">Xiaomi Mi 11 Lite NE đang được phát triển, Mi 11T series sẽ có màn hình OLED</span>
-                                            <span class="post__related__item--time"><i class="fal fa-clock"></i> 2 day agos</span>
-                                        </span>
-                                    </a>
+                                        </a>
+                                        <h3 class="font16 post__related__item--title">
+                                            <a href="javascript:void(0);">Xiaomi Mi 11 Lite NE đang được phát triển, Mi 11T series sẽ có màn hình OLED</a>
+                                        </h3>
+                                        <p class="post__related__item--time font12"><i class="fal fa-clock"></i> 2 day agos</p>
+                                        <p class="post__related__item--caption font14">ASUS đã công bố ROG Phone 3 vào nửa đầu năm 2020. Trong nửa cuối năm, hãng đã trình làng...</p>
+                                    </div>
                                 @endfor
                             </div>
                         </div>
