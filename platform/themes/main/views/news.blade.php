85d424256a11254477dc0499237f60e66df6c27b
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-08-10 22:18:35 +0700

    -

--- platform/themes/main/views/news.blade.php
+++ platform/themes/main/views/news.blade.php
@@ -1,7 +1,7 @@
 <div class="page-content">
     <div class="news-wrapper">
         <div class="container">
-            <section class="hot-post mb-15">
+            <section class="hot-post box-shadown-sm mb-15 pt-15">
                 <div class="feature-posts">
                     @include('theme.main::views.templates.news.feature-posts')
                 </div>
@@ -10,7 +10,7 @@
                 </div>
             </section>
 
-            <section class="list-posts mb-15">
+            <section class="list-posts box-shadown-sm mb-15">
                 @include('theme.main::views.templates.news.list-posts')
             </section>
         </div>
--- platform/themes/main/views/templates/news/population-posts.blade.php
+++ platform/themes/main/views/templates/news/population-posts.blade.php
@@ -2,15 +2,17 @@
     <div class="population__list">
         @for ($i = 0; $i <6 ; $i++)
             <div class="population__item">
+                <div class="population__item--content">
+                    <h3 class="font14 population__item--title">
+                        <a href="javascript:void(0);">
+                            Cận cảnh Motorola Edge S Pro: Smartphone “phá đảo” phân khúc dưới 9 triệu đồng
+                        </a>
+                    </h3>
+                    <p class="population__item--time font12"><i class="fal fa-clock"></i> 2 day agos</p>
+                </div>
                 <a class="population__item--img" href="javascript:void(0);">
                     <img alt="News" class="img-fluid w-100" src="{{ Theme::asset()->url("images/population.jpg") }}">
                 </a>
-                <h3 class="font16 population__item--title">
-                    <a href="javascript:void(0);">
-                        Cận cảnh Motorola Edge S Pro: Smartphone “phá đảo” phân khúc dưới 9 triệu đồng
-                    </a>
-                </h3>
-                <p class="population__item--time font12"><i class="fal fa-clock"></i> 2 day agos</p>
             </div>
         @endfor
     </div>
