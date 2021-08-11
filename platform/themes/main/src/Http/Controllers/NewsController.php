cb642c74c95b876c82b45d8ce58ca152af631e2d
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-08-12 01:02:55 +0700

    -

--- platform/themes/main/src/Http/Controllers/NewsController.php
+++ platform/themes/main/src/Http/Controllers/NewsController.php
@@ -3,9 +3,11 @@
 namespace Theme\Main\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Platform\Blog\Models\Post;
 use Platform\Blog\Repositories\Interfaces\CategoryInterface;
 use Platform\Blog\Repositories\Interfaces\PostInterface;
 use Platform\Blog\Repositories\Interfaces\TagInterface;
+use Platform\Slug\Repositories\Interfaces\SlugInterface;
 use Theme;
 use SeoHelper;
 
@@ -14,17 +16,20 @@ class NewsController extends Controller
     protected $postRepository;
     protected $categoryRepository;
     protected $tagRepository;
+    protected $slugRepository;
 
     /**
      * @param PostInterface $postRepository
      * @param CategoryInterface $categoryRepository
      * @param TagInterface $tagRepository
+     * @param SlugInterface $slugRepository
      */
-    public function __construct(PostInterface $postRepository, CategoryInterface $categoryRepository, TagInterface $tagRepository)
+    public function __construct(PostInterface $postRepository, CategoryInterface $categoryRepository, TagInterface $tagRepository, SlugInterface $slugRepository)
     {
         $this->postRepository = $postRepository;
         $this->categoryRepository = $categoryRepository;
         $this->tagRepository = $tagRepository;
+        $this->slugRepository = $slugRepository;
     }
 
 
@@ -40,10 +45,30 @@ class NewsController extends Controller
         return Theme::scope("news", $data)->render();
     }
 
-    public function getDetail()
+    public function getDetail($slug)
     {
         $data = [];
-        Theme::breadcrumb()->add('Tin tức', 'https://google.com.vn')->add('Thị trường', 'https://:facebook.com');
+
+        $slug = $this->slugRepository->getFirstBy(['key' => $slug, 'reference_type' => Post::class]);
+
+        if (!$slug) {
+            abort(404);
+        }
+
+        $post = $this->postRepository->getFirstBy(['id' => $slug->reference_id], ['*'], ['slugable', 'tags', 'categories']);
+
+        if (!$post) {
+            abort(404);
+        }
+
+        Theme::breadcrumb()->add('Trang chủ', route('public.index'))->add('Tin tức', route('news.index'))->add($post->name, $post->url);
+
+        SeoHelper::setTitle(@$post->name)->setDescription(@$post->description);
+
+        $data['post'] = $post;
+        $data['relateds'] = $this->postRepository->getRelated($post->id, 6);
+        $data['recents'] = $this->postRepository->getRecentPosts(6);
+
         return Theme::scope("news-detail", $data)->render();
     }
 }
