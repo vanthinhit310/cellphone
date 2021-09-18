78fbc557d9845824768a8d36bf7bf7eadf62c233
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-18 13:42:02 +0700

    - custom ant theme

--- platform/themes/bfs/vue/core/layouts/Partials/Header.vue
+++ platform/themes/bfs/vue/core/layouts/Partials/Header.vue
@@ -1,34 +1,41 @@
 <template>
-    <header class="header-wrapper">
-        <div class="container">
-            <div class="top-header">
+    <section class="header-wrapper">
+        <div class="top-header">
+            <div class="container">
                 <a-row type="flex" justify="space-between">
                     <a-col :span="12">
                         <a-space :size="5">
                             <a-button type="link" size="small">
-                                <a-icon type="facebook" /> Facebook
+                                <a-icon type="facebook"/>
+                                Facebook
                             </a-button>
                             <a-button type="link" size="small">
-                                <a-icon type="instagram" /> Instagram
+                                <a-icon type="instagram"/>
+                                Instagram
                             </a-button>
                         </a-space>
                     </a-col>
                     <a-col class="text-right" :span="12">
                         <a-space :size="5">
                             <a-button type="link" size="small">
-                                <a-icon type="environment" /> Tìm cửa hàng
+                                <a-icon type="environment"/>
+                                Tìm cửa hàng
                             </a-button>
                         </a-space>
                     </a-col>
                 </a-row>
             </div>
         </div>
-    </header>
+        <div class="bottom-header primary-bg">
+            <div class="container">
+                <h1>hello</h1>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style scoped>
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,17 +6,28 @@ mix.webpackConfig({
         alias: {
             "@": __dirname + "/platform/themes/bfs/vue",
             "@core": __dirname + "/platform/themes/bfs/vue/core",
-            "@modules": __dirname + "/platform/themes/bfs/vue/modules"
-        }
-    }
+            "@modules": __dirname + "/platform/themes/bfs/vue/modules",
+        },
+    },
 });
-// mix.disableNotifications();
+
 mix.version().webpackConfig({
     output: {
-        chunkFilename: "js/chunks/[name].[hash].js"
-    }
+        chunkFilename: "js/chunks/[name].[hash].js",
+    },
 });
 
 mix.setPublicPath("public")
-    .js("platform/themes/bfs/vue/core/app.js", "public/themes/bfs/js/app.js").vue()
-    .sass("platform/themes/bfs/assets/sass/app.scss", "public/themes/bfs/css/app.css");
+    .js("platform/themes/bfs/vue/core/app.js", "public/themes/bfs/js/app.js")
+    .sass(
+        "platform/themes/bfs/assets/sass/app.scss",
+        "public/themes/bfs/css/app.css"
+    )
+    .less("platform/themes/bfs/assets/less/app.less", "public/themes/bfs/css", {
+        lessOptions: {
+            javascriptEnabled: true,
+        },
+    })
+    .vue();
+
+// mix.disableNotifications();
