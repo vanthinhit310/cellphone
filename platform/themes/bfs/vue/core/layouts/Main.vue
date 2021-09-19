2a2ea7cc2d88e2e3e0b7fd1a28c3dfba7a4261c4
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-20 03:41:59 +0700

    -

--- platform/themes/bfs/vue/core/layouts/Main.vue
+++ platform/themes/bfs/vue/core/layouts/Main.vue
@@ -1,17 +1,13 @@
 <template>
     <section class="main-wrapper">
         <a-layout :style="{ background: '#fff' }">
-            <a-layout-header
-                :style="{ position: 'fixed', zIndex: 20, width: '100%' }"
-            >
+            <a-layout-header :style="{ position: 'fixed', zIndex: 20, width: '100%' }">
                 <Header />
             </a-layout-header>
             <a-layout-content :style="{ marginTop: '106px' }">
                 <router-view />
             </a-layout-content>
-            <!--            <a-layout-footer>-->
-            <!--                Footer-->
-            <!--            </a-layout-footer>-->
+            <a-layout-footer> Footer </a-layout-footer>
         </a-layout>
         <a-back-top />
     </section>
@@ -22,9 +18,9 @@ import Header from "@core/layouts/Partials/Header";
 
 export default {
     components: {
-        Header,
+        Header
     },
-    mounted() {},
+    mounted() {}
 };
 </script>
 
