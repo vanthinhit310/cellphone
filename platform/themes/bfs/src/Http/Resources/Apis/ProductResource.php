f1e9f52c667e59a79008f329174c3b65e3391f42
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-20 00:35:47 +0700

    -

--- platform/themes/bfs/src/Http/Resources/Apis/ProductResource.php
+++ platform/themes/bfs/src/Http/Resources/Apis/ProductResource.php
@@ -12,17 +12,13 @@ class ProductResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->description,
-            'order' => $this->order,
             'slug' => $this->slug,
-            'price' => $this->price,
             'price_formated' => format_price($this->front_sale_price_with_taxes),
             'discount_text' => get_sale_text($this->price, $this->sale_price),
+            'rate_start' => (float)get_average_star_of_product($this->id),
             'sale_price' => $this->sale_price,
             'image' => RvMedia::getImageUrl($this->image, 'product-thumb', false, RvMedia::getDefaultImage()),
             'percentage_off' => (float) get_sale_percentage($this->price, $this->front_sale_price, true, false),
-            'price_with_taxes' => $this->price_with_taxes,
-            'front_sale_price' => $this->front_sale_price,
         ];
     }
 }
--- platform/themes/bfs/vue/modules/Home/Screens/Components/FeaturedProducts.vue
+++ platform/themes/bfs/vue/modules/Home/Screens/Components/FeaturedProducts.vue
@@ -22,29 +22,32 @@
                                         <div class="product-item__content--name">
                                             <span>{{ _.get(item, "name") }}</span>
                                         </div>
-                                        <div class="discount-text">
-                                            <div class="discount-text-content" v-show="_.get(item, 'discount_text')">
-                                                <svg class="svg_icon" viewBox="-0.5 -0.5 4 16">
-                                                    <path
-                                                        d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3"
-                                                        stroke-width="1"
-                                                        transform=""
-                                                        stroke="currentColor"
-                                                        fill="#f69113"></path>
-                                                </svg>
-                                                <div class="text">Giảm {{ _.get(item, 'discount_text') }}</div>
-                                                <svg class="svg_icon" viewBox="-0.5 -0.5 4 16">
-                                                    <path
-                                                        d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3"
-                                                        stroke-width="1"
-                                                        transform="rotate(180) translate(-3 -15)"
-                                                        stroke="currentColor"
-                                                        fill="#f69113"></path>
-                                                </svg>
+                                        <div class="rate-discount">
+                                            <div class="discount-text">
+                                                <div class="discount-text-content" v-show="_.get(item, 'discount_text')">
+                                                    <svg class="svg_icon" viewBox="-0.5 -0.5 4 16">
+                                                        <path
+                                                            d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3"
+                                                            stroke-width="1"
+                                                            transform=""
+                                                            stroke="currentColor"
+                                                            fill="#f69113"></path>
+                                                    </svg>
+                                                    <div class="text">Giảm {{ _.get(item, 'discount_text') }}</div>
+                                                    <svg class="svg_icon" viewBox="-0.5 -0.5 4 16">
+                                                        <path
+                                                            d="M4 0h-3q-1 0 -1 1a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3v0.333a1.2 1.5 0 0 1 0 3q0 1 1 1h3"
+                                                            stroke-width="1"
+                                                            transform="rotate(180) translate(-3 -15)"
+                                                            stroke="currentColor"
+                                                            fill="#f69113"></path>
+                                                    </svg>
+                                                </div>
                                             </div>
+                                            <div class="rate"><a-rate :defaultValue="_.get(item, 'rate_start', 0)" disabled allow-half /></div>
                                         </div>
                                         <div class="product-item__content--price">
-                                            <span class="price">{{ _.get(item, "price_formated") }}</span>
+                                            <div class="price">{{ _.get(item, "price_formated") }}</div>
                                         </div>
                                     </div>
                                 </a>
