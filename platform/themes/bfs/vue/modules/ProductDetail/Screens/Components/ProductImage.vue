c2981c2912ff48fcc6689ca2ee065e8e492e8f1c
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-23 00:47:19 +0700

    -

--- platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductImage.vue
+++ platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductImage.vue
@@ -2,12 +2,12 @@
     <div class="product-imgs">
         <div class="product-imgs-content">
             <div class="relative-background show-image">
-                <div class="background-img" :style="{ backgroundImage: `url(https://picsum.photos/id/10/1200/1200)` }"></div>
+                <div class="background-img" :style="{ backgroundImage: `url(${activeImage})` }"></div>
             </div>
 
             <div class="product-imgs-thumb">
                 <VueSlickCarousel v-bind="settings">
-                    <div v-for="index in 10" :key="index" class="product-thumb">
+                    <div v-for="index in 10" @click="setActiveImage(`https://picsum.photos/id/${index + 1}/320/320`, index)" :key="index" :class="index === activeEl ? 'active product-thumb' : 'product-thumb'">
                         <div class="product-thumb-content">
                             <div class="product-thumb-background" :style="{ backgroundImage: `url(https://picsum.photos/id/${index + 1}/320/320)` }"></div>
                         </div>
@@ -22,6 +22,7 @@
 import VueSlickCarousel from "vue-slick-carousel";
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
 import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
 export default {
     name: "ProductImage",
     components: {
@@ -29,13 +30,21 @@ export default {
     },
     data() {
         return {
+            activeEl: 1,
+            activeImage: "https://picsum.photos/id/10/1200/1200",
             settings: {
                 infinite: false,
-                speed: 500,
+                speed: 800,
                 slidesToShow: 5,
-                slidesToScroll: 1
+                slidesToScroll: 5
             }
         };
+    },
+    methods: {
+        setActiveImage(src, el) {
+            this.activeImage = src;
+            this.activeEl = el;
+        }
     }
 };
 </script>
