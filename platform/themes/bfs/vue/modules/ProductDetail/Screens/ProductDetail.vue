22ebd15de8c10965b9db8d1d5eefe4e8f30ec6f6
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-24 00:05:43 +0700

    -

--- platform/themes/bfs/vue/modules/ProductDetail/Screens/ProductDetail.vue
+++ platform/themes/bfs/vue/modules/ProductDetail/Screens/ProductDetail.vue
@@ -58,39 +58,143 @@
                             <div class="box-title-content">
                                 <div class="title">MÔ TẢ SẢN PHẨM</div>
                                 <div class="content">
-                                    <div>Loại máy xay: Máy xay sinh tố
-                                        Công suất: 450W
-                                        Chức năng chính: Xay sinh tố, Xay hạt
-                                        Cối xay: 2 cối, Cối lớn: 1L, cối nhỏ: 0.2L
-                                        Chất liệu cối xay: Nhựa
-                                        Chất liệu lưỡi dao: Thép không rỉ
-                                        Tốc độ: 1 tốc độ, 1 nút nhồi để trộn nguyên liệu
-                                        Nút chỉnh tốc độ: Nút nhấn
-                                        Tính năng an toàn: Chân đế chống trượt, Máy chỉ hoạt động khi lắp cối vừa thân máy, Tự ngắt khi quá tải<h5>Thiết kế đơn giản, tiện dụng</h5>
-                                        <p><strong>Máy Xay Sinh Tố Philips HR2056 (450W)</strong>&nbsp;sở hữu thiết kế gọn gàng, dễ di chuyển khi sử dụng hay cất giữ. Cùng với màu sắc trang nhã, sản phẩm làm đẹp thêm cho không gian nhà bếp.</p>
-                                        <p><img src="https://salt.tikicdn.com/ts/tmp/63/a5/2b/6b644147f2a1400ffac32b226b41e7b0.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="1026"></p>
+                                    <div>
+                                        <p>
+                                            Loại máy xay: Máy xay sinh tố Công suất: 450W Chức năng chính: Xay sinh tố, Xay hạt Cối xay: 2 cối, Cối lớn: 1L, cối nhỏ: 0.2L Chất liệu cối xay: Nhựa Chất
+                                            liệu lưỡi dao: Thép không rỉ Tốc độ: 1 tốc độ, 1 nút nhồi để trộn nguyên liệu Nút chỉnh tốc độ: Nút nhấn Tính năng an toàn: Chân đế chống trượt, Máy chỉ
+                                            hoạt động khi lắp cối vừa thân máy, Tự ngắt khi quá tải
+                                        </p>
+                                        <h5>Thiết kế đơn giản, tiện dụng</h5>
+                                        <p>
+                                            <strong>Máy Xay Sinh Tố Philips HR2056 (450W)</strong>&nbsp;sở hữu thiết kế gọn gàng, dễ di chuyển khi sử dụng hay cất giữ. Cùng với màu sắc trang nhã, sản
+                                            phẩm làm đẹp thêm cho không gian nhà bếp.
+                                        </p>
+                                        <p>
+                                            <img src="https://salt.tikicdn.com/ts/tmp/63/a5/2b/6b644147f2a1400ffac32b226b41e7b0.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="1026" />
+                                        </p>
                                         <h5>Công suất tối đa 450W</h5>
-                                        <p>Máy xay Philips HR2056/00 hoạt động với công suất tối đa 450W, cùng lưỡi dao sắc bén bằng thép không gỉ cho bạn xay nhuyễn mọi loại thực phẩm dễ dàng và nhanh chóng, tiết kiệm thời gian mà vẫn đảm bảo chất lượng đồ ăn.</p>
-                                        <p><img src="https://salt.tikicdn.com/ts/tmp/04/0a/ee/41981e0c191d5e77ff59fa7d0446d9fb.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="345"><span style="font-size: small;"><img src="https://salt.tikicdn.com/ts/tmp/3e/2d/2f/60d9e0ca9f0fc2d2dc349f99b74be1d3.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="421"></span></p>
+                                        <p>
+                                            Máy xay Philips HR2056/00 hoạt động với công suất tối đa 450W, cùng lưỡi dao sắc bén bằng thép không gỉ cho bạn xay nhuyễn mọi loại thực phẩm dễ dàng và
+                                            nhanh chóng, tiết kiệm thời gian mà vẫn đảm bảo chất lượng đồ ăn.
+                                        </p>
+                                        <p>
+                                            <img
+                                                src="https://salt.tikicdn.com/ts/tmp/04/0a/ee/41981e0c191d5e77ff59fa7d0446d9fb.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056"
+                                                width="750"
+                                                height="345" /><span style="font-size: small"
+                                                ><img src="https://salt.tikicdn.com/ts/tmp/3e/2d/2f/60d9e0ca9f0fc2d2dc349f99b74be1d3.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="421"
+                                            /></span>
+                                        </p>
                                         <h5>Lưỡi xay 4 cạnh bằng thép không gỉ</h5>
-                                        <p>Thiết kế chân đế chống trơn trượt, máy sẽ bám chắc vào mặt bàn, giữ máy luôn vững vàng ổn định, tránh rung lắc, rơi vỡ khi vận hành giúp xay hiệu quả hơn. Ngoài ra, máy kèm thêm chế độ tự ngắt, sẽ ngưng hoạt động khi quá nóng, xay quá nhiều thực phẩm hay thực phẩm quá cứng để duy trì độ bền của máy cũng như tính an toàn cho người dùng.</p>
-                                        <p><img src="https://salt.tikicdn.com/ts/tmp/9b/6d/04/08e7ab7123e773eaffdda220550cae17.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="417"></p>
+                                        <p>
+                                            Thiết kế chân đế chống trơn trượt, máy sẽ bám chắc vào mặt bàn, giữ máy luôn vững vàng ổn định, tránh rung lắc, rơi vỡ khi vận hành giúp xay hiệu quả hơn.
+                                            Ngoài ra, máy kèm thêm chế độ tự ngắt, sẽ ngưng hoạt động khi quá nóng, xay quá nhiều thực phẩm hay thực phẩm quá cứng để duy trì độ bền của máy cũng như
+                                            tính an toàn cho người dùng.
+                                        </p>
+                                        <p><img src="https://salt.tikicdn.com/ts/tmp/9b/6d/04/08e7ab7123e773eaffdda220550cae17.jpg" alt="Máy Xay Sinh Tố Philips HR2056" width="750" height="417" /></p>
                                         <h5>Trang bị cối xây khô tiện dụng</h5>
-                                        <p>Philips HR2056 được trang bị thêm cối xay thực phẩm khô 0.2 lít, dùng để xay tiêu, cà phê, các loại hạ giúp công việc chế biến các loại gia vị trở nên dễ dàng hơn. Bên cạnh đó, cối có thể dùng để xay thịt (50 g / lần xay và phải cắt nhỏ) nhưng không chuyên dùng, không nên xay thịt thường xuyên sẽ ảnh hưởng tới độ bền cũng như độ sắc bén của lưỡi dao.</p>
+                                        <p>
+                                            Philips HR2056 được trang bị thêm cối xay thực phẩm khô 0.2 lít, dùng để xay tiêu, cà phê, các loại hạ giúp công việc chế biến các loại gia vị trở nên dễ
+                                            dàng hơn. Bên cạnh đó, cối có thể dùng để xay thịt (50 g / lần xay và phải cắt nhỏ) nhưng không chuyên dùng, không nên xay thịt thường xuyên sẽ ảnh hưởng
+                                            tới độ bền cũng như độ sắc bén của lưỡi dao.
+                                        </p>
                                         <h5>An toàn cho người dùng</h5>
-                                        <p>Máy xay sinh tố với chế độ tự ngắt điện an toàn đảm bảo máy không hoạt động quá công suất, gia tăng độ bền của sản phẩm cũng như sự an toàn cho người dùng.</p>
-                                        <p>Máy xay sinh tố Philips HR2056 thương hiệu Philips uy tín của Hà Lan được người tiêu dùng tin tưởng và ưa chuộng từ lâu, đảm bảo mang lại sự hài lòng cao nhất cho khách hàng khi chọn mua và sử dụng sản phẩm.</p>
+                                        <p>
+                                            Máy xay sinh tố với chế độ tự ngắt điện an toàn đảm bảo máy không hoạt động quá công suất, gia tăng độ bền của sản phẩm cũng như sự an toàn cho người dùng.
+                                        </p>
+                                        <p>
+                                            Máy xay sinh tố Philips HR2056 thương hiệu Philips uy tín của Hà Lan được người tiêu dùng tin tưởng và ưa chuộng từ lâu, đảm bảo mang lại sự hài lòng cao
+                                            nhất cho khách hàng khi chọn mua và sử dụng sản phẩm.
+                                        </p>
                                         <h5>Một số hình ảnh thực tế sản phẩm</h5>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/da/6f/0d/ddc708407a5c0a9a6e82a44d69f50b57.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/da/6f/0d/ddc708407a5c0a9a6e82a44d69f50b57.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/13/9f/46/403bccf959e42d24c8c4e261ebd33b1a.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/13/9f/46/403bccf959e42d24c8c4e261ebd33b1a.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/3c/a9/4b/77ca722c9c1a91011afa43f6a7a18d9c.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/3c/a9/4b/77ca722c9c1a91011afa43f6a7a18d9c.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/e4/d4/34/eaa09557bba3a47fe54ea56a5b533a15.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/e4/d4/34/eaa09557bba3a47fe54ea56a5b533a15.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/ba/8c/b2/4ce95bc9764f3da5218691026282e07a.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/ba/8c/b2/4ce95bc9764f3da5218691026282e07a.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/c1/a1/71/4e4f3e57c248371728b571fa305cf235.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/c1/a1/71/4e4f3e57c248371728b571fa305cf235.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/73/33/b2/a5b501c10cbe76b1981a0ea5ca9007c3.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/73/33/b2/a5b501c10cbe76b1981a0ea5ca9007c3.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/42/7a/88/d50fb7d99f8f893885bb9aa654a5a649.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/42/7a/88/d50fb7d99f8f893885bb9aa654a5a649.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p><img id="https://salt.tikicdn.com/ts/tmp/b3/aa/7f/5c1cd96f14d8e5566e7f42df4ac123b6.jpg" title="Máy Xay Sinh Tố Philips HR2056 (350W)" src="https://salt.tikicdn.com/ts/tmp/b3/aa/7f/5c1cd96f14d8e5566e7f42df4ac123b6.jpg" alt="Máy Xay Sinh Tố Philips HR2056 (350W)" width="750" height="500"></p>
-                                        <p>&nbsp;</p><p>Giá sản phẩm trên Tiki đã bao gồm thuế theo luật hiện hành. Tuy nhiên tuỳ vào từng loại sản phẩm hoặc phương thức, địa chỉ giao hàng mà có thể phát sinh thêm chi phí khác như phí vận chuyển, phụ phí hàng cồng kềnh, .....</p></div>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/da/6f/0d/ddc708407a5c0a9a6e82a44d69f50b57.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/da/6f/0d/ddc708407a5c0a9a6e82a44d69f50b57.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/13/9f/46/403bccf959e42d24c8c4e261ebd33b1a.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/13/9f/46/403bccf959e42d24c8c4e261ebd33b1a.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/3c/a9/4b/77ca722c9c1a91011afa43f6a7a18d9c.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/3c/a9/4b/77ca722c9c1a91011afa43f6a7a18d9c.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/e4/d4/34/eaa09557bba3a47fe54ea56a5b533a15.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/e4/d4/34/eaa09557bba3a47fe54ea56a5b533a15.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/ba/8c/b2/4ce95bc9764f3da5218691026282e07a.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/ba/8c/b2/4ce95bc9764f3da5218691026282e07a.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/c1/a1/71/4e4f3e57c248371728b571fa305cf235.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/c1/a1/71/4e4f3e57c248371728b571fa305cf235.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/73/33/b2/a5b501c10cbe76b1981a0ea5ca9007c3.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/73/33/b2/a5b501c10cbe76b1981a0ea5ca9007c3.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/42/7a/88/d50fb7d99f8f893885bb9aa654a5a649.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/42/7a/88/d50fb7d99f8f893885bb9aa654a5a649.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>
+                                            <img
+                                                id="https://salt.tikicdn.com/ts/tmp/b3/aa/7f/5c1cd96f14d8e5566e7f42df4ac123b6.jpg"
+                                                title="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                src="https://salt.tikicdn.com/ts/tmp/b3/aa/7f/5c1cd96f14d8e5566e7f42df4ac123b6.jpg"
+                                                alt="Máy Xay Sinh Tố Philips HR2056 (350W)"
+                                                width="750"
+                                                height="500" />
+                                        </p>
+                                        <p>&nbsp;</p>
+                                        <p>
+                                            Giá sản phẩm trên Tiki đã bao gồm thuế theo luật hiện hành. Tuy nhiên tuỳ vào từng loại sản phẩm hoặc phương thức, địa chỉ giao hàng mà có thể phát sinh
+                                            thêm chi phí khác như phí vận chuyển, phụ phí hàng cồng kềnh, .....
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
