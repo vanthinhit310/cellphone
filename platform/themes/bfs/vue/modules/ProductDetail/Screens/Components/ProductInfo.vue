2f0e204b99b9749f98fe0590f67fbfbbeeed57c8
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-24 00:08:28 +0700

    -

--- platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductInfo.vue
+++ platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductInfo.vue
@@ -26,12 +26,18 @@
             <div class="product-attributes product-info-label">
                 <label class="label">Màu sắc</label>
                 <div class="content">
-                    <button :data-variation="index" v-for="index in 10" :key="index" :class="(active === index) ? 'product-variation product-variation--selected' : 'product-variation'">
+                    <button
+                        @click="setActive(index)"
+                        :data-variation="index"
+                        v-for="index in 10"
+                        :key="index"
+                        :class="active === index ? 'product-variation product-variation--selected' : 'product-variation'">
                         Thường 2,7m
-                        <div v-show="(active === index)" class="product-variation__tick">
+                        <div v-show="active === index" class="product-variation__tick">
                             <svg enable-background="new 0 0 12 12" viewBox="0 0 12 12" x="0" y="0" class="shopee-svg-icon icon-tick-bold">
                                 <g>
-                                    <path d="m5.2 10.9c-.2 0-.5-.1-.7-.2l-4.2-3.7c-.4-.4-.5-1-.1-1.4s1-.5 1.4-.1l3.4 3 5.1-7c .3-.4 1-.5 1.4-.2s.5 1 .2 1.4l-5.7 7.9c-.2.2-.4.4-.7.4 0-.1 0-.1-.1-.1z"></path>
+                                    <path
+                                        d="m5.2 10.9c-.2 0-.5-.1-.7-.2l-4.2-3.7c-.4-.4-.5-1-.1-1.4s1-.5 1.4-.1l3.4 3 5.1-7c .3-.4 1-.5 1.4-.2s.5 1 .2 1.4l-5.7 7.9c-.2.2-.4.4-.7.4 0-.1 0-.1-.1-.1z"></path>
                                 </g>
                             </svg>
                         </div>
@@ -77,6 +83,9 @@ export default {
         };
     },
     methods: {
+        setActive(index) {
+            this.active = index;
+        },
         increment() {
             if (this.quantity === this.max) {
                 this.quantity = this.max;
