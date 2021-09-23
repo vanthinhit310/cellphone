f5dec372ce86e3cbaae732d31dbae71bee823f23
Lê Văn Thịnh <39481937+vanthinhit310@users.noreply.github.com>
2021-09-24 01:07:33 +0700

    -

--- platform/themes/bfs/vue/modules/ProductDetail/Store/actions.js
+++ platform/themes/bfs/vue/modules/ProductDetail/Store/actions.js
@@ -1,5 +1,7 @@
 import axios from "@core/configs/axiosConfig";
 
 export default {
-
+    async getProduct({commit, state, dispatch}, slug) {
+        return await axios.get(`products/${slug}`);
+    },
 };
