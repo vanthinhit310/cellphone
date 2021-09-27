7b8810d543a9807260e17b9187ceabe2d7511029
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-09-28 00:07:59 +0700

    -

--- platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductImage.vue
+++ platform/themes/bfs/vue/modules/ProductDetail/Screens/Components/ProductImage.vue
@@ -1,14 +1,22 @@
 <template>
     <a-spin :spinning="processing">
-        <a-icon slot="indicator" type="loading" style="font-size: 24px" spin />
+        <a-icon slot="indicator" type="loading" style="font-size: 24px" spin/>
         <div class="product-imgs">
             <div class="product-imgs-content">
                 <div class="relative-background show-image">
-                    <div v-if="_.get(product, 'thumbnail')" class="background-img" :style="{ backgroundImage: `url(${activeImage})` }"></div>
+                    <div v-if="_.get(product, 'thumbnail')" @click="lightboxIndex = activeEl" class="background-img" :style="{ backgroundImage: `url(${activeImage})` }"></div>
                 </div>
 
                 <template v-if="images.length">
                     <div class="product-imgs-thumb">
+                        <CoolLightBox
+                            :items="big_images"
+                            :index="lightboxIndex"
+                            :effect="'fade'"
+                            :fullScreen="true"
+                            :useZoomBar="true"
+                            @close="lightboxIndex = null">
+                        </CoolLightBox>
                         <VueSlickCarousel v-bind="settings">
                             <div v-for="(item, index) in images" @click="setActiveImage(item, index)" :key="index" :class="index === activeEl ? 'active product-thumb' : 'product-thumb'">
                                 <div class="product-thumb-content">
@@ -23,16 +31,16 @@
                     <a-space :size="15">
                         <span>Chia sẻ:</span>
                         <a class="social-item" type="button">
-                            <img class="img-fluid" alt="Facebook" src="/themes/bfs/images/facebook.png" />
+                            <img class="img-fluid" alt="Facebook" src="/themes/bfs/images/facebook.png"/>
                         </a>
                         <a class="social-item" type="button">
-                            <img class="img-fluid" alt="Zalo" src="/themes/bfs/images/zalo.png" />
+                            <img class="img-fluid" alt="Zalo" src="/themes/bfs/images/zalo.png"/>
                         </a>
                         <a class="social-item" type="button">
-                            <img class="img-fluid" alt="Pinterest" src="/themes/bfs/images/pinterest.png" />
+                            <img class="img-fluid" alt="Pinterest" src="/themes/bfs/images/pinterest.png"/>
                         </a>
                         <a class="social-item" type="button">
-                            <img class="img-fluid" alt="Twitter" src="/themes/bfs/images/twitter.png" />
+                            <img class="img-fluid" alt="Twitter" src="/themes/bfs/images/twitter.png"/>
                         </a>
                     </a-space>
                 </div>
@@ -45,11 +53,14 @@
 import VueSlickCarousel from "vue-slick-carousel";
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
 import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import CoolLightBox from 'vue-cool-lightbox'
+import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
 
 export default {
     name: "ProductImage",
     components: {
-        VueSlickCarousel
+        VueSlickCarousel,
+        CoolLightBox
     },
     props: {
         productDetail: String | Object
@@ -59,6 +70,8 @@ export default {
             activeEl: 0,
             activeImage: "",
             images: [],
+            big_images: [],
+            lightboxIndex: null,
             product: "",
             processing: true,
             settings: {
@@ -80,6 +93,7 @@ export default {
             this.product = this.productDetail;
             this.activeImage = _.get(this.productDetail, "thumbnail");
             this.images = _.get(this.productDetail, "small_images");
+            this.big_images = _.get(this.productDetail, "big_images");
             this.processing = false;
         }
     }
