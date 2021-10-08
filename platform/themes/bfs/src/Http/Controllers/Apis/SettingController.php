7aeda55abc52584f730ce8262eb23101d6231ebc
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-10-08 22:16:32 +0700

    Update SettingController.php

--- platform/themes/bfs/src/Http/Controllers/Apis/SettingController.php
+++ platform/themes/bfs/src/Http/Controllers/Apis/SettingController.php
@@ -26,14 +26,14 @@ class SettingController extends Controller
         $settings = [
             "ecommerce_store_name" => setting('ecommerce_store_name'),
             "ecommerce_store_phone" => setting('ecommerce_store_phone'),
-            "theme--shop_map" => setting('theme--shop_map'),
-            "theme--shop_facebook_link" => setting('theme--shop_facebook_link'),
-            "theme--shop_zalo_link" => setting('theme--shop_zalo_link'),
-            "theme--shop_email" => setting('theme--shop_email'),
-            "theme--logo" => RvMedia::getImageUrl(setting('theme--logo')),
-            "theme--home_banner_1" => RvMedia::getImageUrl(setting('theme--home_banner_1')),
-            "theme--home_banner_2" => RvMedia::getImageUrl(setting('theme--home_banner_2')),
-            "theme--copyright" => setting('theme--copyright'),
+            "theme--shop_map" => theme_option('shop_map'),
+            "theme--shop_facebook_link" => theme_option('shop_facebook_link'),
+            "theme--shop_zalo_link" => theme_option('shop_zalo_link'),
+            "theme--shop_email" => theme_option('shop_email'),
+            "theme--logo" => RvMedia::getImageUrl(theme_option('logo')),
+            "theme--home_banner_1" => RvMedia::getImageUrl(theme_option('home_banner_1')),
+            "theme--home_banner_2" => RvMedia::getImageUrl(theme_option('home_banner_2')),
+            "theme--copyright" => theme_option('copyright'),
         ];
         return response()->json(["settings" => $settings], Response::HTTP_OK);
     }
